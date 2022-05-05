78d2b8433bcf5cb83c9bede5448fe9e5d76ccf62
GiuliaV <valente.giulia93@gmail.com>
2022-05-05 16:33:04 +0200

    Milestone2 - script visibile a video

--- script/script.js
+++ script/script.js
@@ -3,30 +3,41 @@
 // MILESTONE 1
 // Per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
 
+// Definisco la variabile container associandola al corrispettivo elemento HTML
+const div = document.querySelector("div.container");
+
 // 1. Genera numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
-
+    
     // 2. Stampa FizzBuzz se multiplo di 3 e 5
 
     if((i%3 == 0) && (i%5 == 0)){
         console.log ("FizzBuzz");
+        const element=`<div class="FizzBuzz container--${i}"> FizzBuzz </div>`;
+        div.innerHTML += element;
     }
 
     // 3. Stampa Fizz se multipplo di 3
 
     else if(i%3 == 0){
         console.log ("Fizz");
+        const element=`<div class="Fizz container--${i}"> Fizz </div>`;
+        div.innerHTML += element;
     }
 
     // 4. Stampa Buzz se multipplo di 5
 
     else if ((i%5 == 0)){
         console.log ("Buzz");
+        const element=`<div class="Buzz container--${i}"> Buzz </div>`;
+        div.innerHTML += element;
     }
 
     // 5. Scrive i numeri in console
 
     else {
         console.log (i);
+        const element=`<div class="container container--${i}"> ${i} </div>`;
+        div.innerHTML += element;
     }
 }
