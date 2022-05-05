f26f577680dc34c9e3e356be8939ea7ca86c2195
GiuliaV <valente.giulia93@gmail.com>
2022-05-05 17:30:48 +0200

    modificato script (else che crea box con numeri)

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-
 // Scrivi un programma che stampi in console i numeri da 1 a 100.
 // MILESTONE 1
 // Per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
@@ -37,7 +36,7 @@ for (let i = 1; i <= 100; i++){
 
     else {
         console.log (i);
-        const element=`<div class="box container--${i}">${i}</div>`;
+        const element=`<div class="box --${i}">${i}</div>`;
         div.innerHTML += element;
     }
 }
